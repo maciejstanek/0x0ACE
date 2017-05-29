b4badd808d766773cefb189b536760bebcf01c35
maciejstanek <makaron28@gmail.com>
2017-05-29 17:50:31 +0200

    wip

--- 03_labirynth/req.sh
+++ 03_labirynth/req.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 ip="[2a01:4f8:160:5263::e3d3:467f]:2766"
-#game_key="X-0x0ACE-Key: PKmAZq8oYagM1R6N2ZOlxzJbknvpVXJe9V3q0wyQAe5LKj8W4EPG9dDrm0jW6VNL"
-game_key="X-0x0ACE-Key: yDRWEpJRJ9WpV0DEzeA1rQLONgKyo7dVvP3wdYMm2Glb6jxakZv4qn5P85Ldg60n"
+#game_key=PKmAZq8oYagM1R6N2ZOlxzJbknvpVXJe9V3q0wyQAe5LKj8W4EPG9dDrm0jW6VNLi
+game_key=yDRWEpJRJ9WpV0DEzeA1rQLONgKyo7dVvP3wdYMm2Glb6jxakZv4qn5P85Ldg60n
+game_key_header="X-0x0ACE-Key: $game_key"
+echo -e "\e[46;1m$game_key_header\e[0m"
 html_file=dump.html
 
 # Scrap the current html page
-curl -6 --header "$game_key" $ip > $html_file
+curl -6 --header "$game_key_header" --data "X-0x0ACE-Key=$game_key" $ip > $html_file
 
-echo -e "\e[1;41m\n\e[0m"
+#echo -e "\e[1;41m\n\e[0m"
 cat $html_file
 

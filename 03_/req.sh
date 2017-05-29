9f46bcbe95b4e9da3645a3fdedf8569bbe0fc6b6
maciej-stanek <makaron28@gmail.com>
2017-05-29 15:17:11 +0200

    wip

---
+++ 03_/req.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ip="[2a01:4f8:160:5263::e3d3:467f]:2766"
+game_key="X-0x0ACE-Key: PKmAZq8oYagM1R6N2ZOlxzJbknvpVXJe9V3q0wyQAe5LKj8W4EPG9dDrm0jW6VNL"
+html_file=dump.html
+
+# Scrap the current html page
+curl -6 --header "$game_key" $ip > $html_file
+
+echo -e "\e[1;41m\n\e[0m"
+cat $html_file
+
